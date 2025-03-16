e3286162c4d224289bfdb2f5665775e5e634d6a0
Jeff <24702178+jmg292@users.noreply.github.com>
2025-03-16 17:43:54 -0500

    Sketch out HTTP API

---
+++ rayhunter/api.py
@@ -0,0 +1,27 @@
+import requests
+import logging
+
+
+class RayhunterApi:
+
+    @property
+    def active_capture(self) -> bool:
+        raise NotImplementedError()
+
+    def __init__(self, hostname: str, port: int):
+        self._url = f"http://{hostname}:{port}/api"
+
+    def get_manifest(self):
+        raise NotImplementedError()
+    
+    def get_pcap_file(self, filename: str):
+        raise NotImplementedError()
+    
+    def get_qmdl_file(self, filename: str):
+        raise NotImplementedError()
+    
+    def start_analysis(self):
+        raise NotImplementedError()
+    
+    def stop_analysis(self):
+        raise NotImplementedError()
